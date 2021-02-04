eb4dda26c746e1e2da46598b7d029bfbe18661f9
spencer tyminski <nynjaorca@gmail.com>
2021-02-03 20:13:09 -0600

    added comments for clarity and removed the fmt print line that said hello

--- cmd/svr/main.go
+++ cmd/svr/main.go
@@ -8,11 +8,16 @@ import (
 
 func main() {
 
+	// creates a new default gin router
 	r := gin.Default()
+
+	// route get is designed to respond back with a 200 and a message of "pong"
+	// so long as the application is reachable
 	r.GET("/ping", func(c *gin.Context) {
 		fmt.Println("pong!")
 		c.JSON(200, gin.H{"message": "pong"})
 	})
+
+	// starts the server and exposes routes to the webs
 	r.Run()
-	fmt.Println("hello!")
 }
