d4732ee4b878815a1ffe95dbabd8101490dbda53
ecklenke <eklenke2@live.maryville.edu>
2021-02-03 19:37:02 -0700

    Moved router functionality to router.go.

--- cmd/svr/main.go
+++ cmd/svr/main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/gin-gonic/gin"
+	"github.com/YOUR-USER-OR-ORG-NAME/YOUR-REPO-NAME/internal/router"
 )
 
 func main() {
-
-	// creates a new default gin router
-	r := gin.Default()
-
-	// route get is designed to respond back with a 200 and a message of "pong"
-	// so long as the application is reachable
-	r.GET("/ping", func(c *gin.Context) {
-		fmt.Println("pong!")
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-
 	// starts the server and exposes routes to the webs
+	r := router.New()
+	router.BuildRoutes(r)
 	r.Run()
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,23 @@
 package router
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func New() *gin.Engine {
+	// creates a new default gin router
+	routerEngine := gin.Default()
+	return routerEngine
+}
+
+func BuildRoutes(ginny *gin.Engine) {
+	// route get is designed to respond back with a 200 and a message of "pong"
+	// so long as the application is reachable
+	ginny.GET("/ping", func(c *gin.Context) {
+		fmt.Println("pong!")
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+}
