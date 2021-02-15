5eef999838e9ed87df6929a8e60e79c580b7831a
nynja <nynjaorca@gmail.com>
2021-02-14 19:53:14 -0600

    removal of auth0 implementation for now. We'll be moving forward with angular first

--- cmd/svr/main.go
+++ cmd/svr/main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"github.com/ecklenke9/SandEWebApp/internal/router"
-)
+import "github.com/ecklenke9/SandEWebApp/internal/router"
 
 func main() {
 	// starts the server and exposes routes to the webs
 	r := router.New()
-	router.BuildRoutes(r)
-	r.Run()
+	r.BuildRoutes()
+	r.Engine.Run()
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/ecklenke9/SandEWebApp
 go 1.14
 
 require (
+	github.com/auth0-community/go-auth0 v1.0.0 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/rs/xid v1.2.1
 	github.com/rs/zerolog v1.20.0
+	gopkg.in/square/go-jose.v2 v2.1.7 // indirect
 )
